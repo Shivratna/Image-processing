dc486e0174284fc248b3248234f798667759a737
= <=>
2023-08-25 00:18:05 +0530

    final

--- day3/main.py
+++ day3/main.py
@@ -10,7 +10,8 @@ def midpoint(A,B):
 
 # Gray and blur it slightly for further process
 
-image = cv2.imread("/Users/Shivratna_pvt/Desktop/Projects/IP Basics/Image-processing/day3/example_02.png")
+image = cv2.imread("example_01.png")
+width = 2
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray,(7,7),0)
 
@@ -27,8 +28,64 @@ cnts = imutils.grab_contours(cnts)
 
 # Sorting contours left to right
 (cnts,_) = contours.sort_contours(cnts)
-ppm = None
+pixelsPerMetric = None
 
 cv2.imshow("Edged",edged)
-cv2.waitKey(0)
+orig = image
+for c in cnts:
+    
+    if cv2.contourArea(c)<100:
+        continue
+    
+    orig = image.copy()
+    box = cv2.minAreaRect(c)
+    box = cv2.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box,dtype=int)
+    
+    box = perspective.order_points(box)
+    cv2.drawContours(orig,[box.astype("int")],-1,(0,255,0),2)
+    
+    for(x,y) in box:
+        cv2.circle(orig,(int(x),int(y)),5,(0,0,255),-1)
+    
+    (tl,tr,br,bl) = box
+    (tltrX,tltrY) = midpoint(tl,tr)
+    (blbrX,blbrY) = midpoint(bl,br)
+    
+    (tlblX,tlblY) = midpoint(tl,bl)
+    (trbrX,trbrY) = midpoint(tr,br)
+    
+    cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+	# draw lines between the midpoints
+    cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+		(255, 0, 255), 2)
+    cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+		(255, 0, 255), 2)
+    	# compute the Euclidean distance between the midpoints
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
+	# if the pixels per metric has not been initialized, then
+	# compute it as the ratio of pixels to supplied metric
+	# (in this case, inches)
+    if pixelsPerMetric is None:
+        pixelsPerMetric = dB / width
+     
+	# compute the size of the object
+    dimA = dA/pixelsPerMetric
+    dimB = dB/pixelsPerMetric
+
+    # draw the object sizes on the image
+    cv2.putText(orig, "{:.1f}in".format(dimA),
+            (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+            0.65, (255, 255, 255), 2)
+    cv2.putText(orig, "{:.1f}in".format(dimB),
+            (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+            0.65, (255, 255, 255), 2)
+
+    # show the output image
+    cv2.imshow("Image", orig)
+    cv2.waitKey(0)
